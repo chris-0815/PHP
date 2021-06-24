51006119ab3e51e17c0c91b6f6c4eca89385b07d
chris-0815 <58494290+chris-0815@users.noreply.github.com>
2021-06-24 13:45:05 +0200

    Update index.php

---
+++ MyShop/index.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors');
+?>
+
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Christoph`s WebShop</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+<head>
+<body>
+
+
+
+
+</body>
+</html>
