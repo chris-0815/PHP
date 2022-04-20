c000b6f8a0fd964e4bce2a503c437a1ddb24d4f9
chris-0815 <58494290+chris-0815@users.noreply.github.com>
2022-04-20 12:31:12 +0200

    usort

--- PHP-LearnIT/Übungen/Arrays/arrays.php
+++ PHP-LearnIT/Übungen/Arrays/arrays.php
@@ -6,7 +6,17 @@ $saetze = [
     11 => "Milch",
 ];
 
+echo nl2br("asort\n");
 asort($saetze);
-
 var_dump($saetze);
+
+echo nl2br("ksort\n");
+ksort($saetze);
+var_dump($saetze);
+
+echo nl2br("usort\n");
+usort($saetze, function($a, $b) {
+    var_dump($a);
+    var_dump($b);
+});
 ?>
